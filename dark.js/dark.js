907a5a0867bb5239b864d21f3b4c2251c78f1bd5
Davi Cabral <davidasilvacabral315@gmail.com>
2024-05-15 16:09:35 -0300

    modo escuro e claro

---
+++ dark.js/dark.js
@@ -0,0 +1,20 @@
+function clar(){
+    document.body.style.backgroundColor = "white"
+    document.body.style.color = "black"
+
+    var btEs = document.getElementById('escuro')
+    var btCla = document.getElementById('claro')
+
+    
+}
+
+function escur(){
+   document.body.style.backgroundColor ="#333333"
+   document.body.style.color = "white"
+   document.body.style.h1 = "white"
+
+   var btEs = document.getElementById('escuro')
+   var btCla = document.getElementById('claro')
+
+   
+}
